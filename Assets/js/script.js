e888e61bd3f391b846fa9ac28e6ebddcdbf85f96
Jeremy Marotta <firefreet@gmail.com>
2020-01-08 19:21:32 -0500

    finished commenting

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -70,56 +70,84 @@ $(document).ready(function () {
         var yyyy = today.getFullYear();
         today = yyyy + '-' + mm + '-' + dd;
 
-
+        // first call to get current weather for specified city
         $.ajax({
             url: currentDataUrl,
             method: "GET",
+            // if call fails, alert user with the error
             error: function(xhr,y){
                 alert("Error: " + xhr.responseJSON.message)
                 },
+            // when call succeeds, do...
             success: function (resp) {
+                // get longituted and latitude coords to call UV index api
                 uvDataUrl = uvDataUrl + "&lat=" + resp.coord.lat + "&lon=" + resp.coord.lon
+                // get city name from response (so display can be correct case)
                 RespCity = resp.name
+                // if city is already in list of previous cities, move it to the top of the array/list
+                
                 if($.inArray(RespCity,cities) !== -1) {
                     cities.splice($.inArray(RespCity,cities),1);
                     cities.unshift(RespCity);
-                } else {cities.unshift(RespCity)}
+                } 
+                // other wise just put it at the top of the list
+                else {cities.unshift(RespCity)}
+                // cap the list at 10 entries
                 cities = cities.slice(0, 10);
-                localStorage.setItem("cities", JSON.stringify(cities));   
+                // save the list to local storageu
+                localStorage.setItem("cities", JSON.stringify(cities));
+                // repopulate list of html buttons with cities   
                 populateCities();
+                // clear out search field text
                 $("#searchCity").val("")
+                // populate current day's weather with ...
+                // City name, date, current weather icon
                 $("#card-today>.city-display").html(RespCity + " (" + today + ")<img src=http://openweathermap.org/img/wn/" + resp.weather[0].icon + ".png>").hide().fadeIn()
+                // Temperature
                 $("#card-today>.temp-display").text("Temperature: " + parseInt(resp.main.temp_min) + degreeSign)
+                // Humitidy
                 $("#card-today>.humid-display").text("Humidity: " + resp.main.humidity + " %")
+                // Wind speed
                 $(".wind-display").text("Wind Speed: " + resp.wind.speed + " MPH")
+                // Feels like temperature
                 $(".feels-display").text("Feels Like: " + parseInt(resp.main.feels_like) + degreeSign)
 
+                // call UV Index API
                 $.ajax({
                     url: uvDataUrl,
                     method: "GET"
                 }).then(function(resp) {
+                    // get UV value
                     var uv = resp.value
+                    // set string for CSS to apply color based on value of UV on scale
                     var color = "background-color: "
                     if(uv < 3) {color += "green; color: white;"} 
                     else if (uv < 6) {color += "yellow; color: black;"}
                     else if (uv < 8) {color += "orange; color: white"}
                     else if (uv < 11) {color += "red; color: white;"}
                     else {color += "purple; color: white;"}
+                    // display UV value and set CSS
                     $(".uv-display>div").text(uv).attr("style",color)
                 })
-    
+                
+                // call weather forecast API
                 $.ajax({
                     url: futureDataUrl,
                     method: "GET"
                 }).then(function (resp) {
+                    // get the array of weather information (which is every 3 hours for 5 days)
                     days = resp.list
+                    // loop 5 times to populate 5 day forecast elements
                     for (i = 0; i < 5; i++) {
-                        e = 3 + (i*8)
+                        // transform iterator to get the noon time forecasts from the array (@ 3, 11, 19, 27, 35)
+                        noon = 3 + (i*8)
+                        // string to select children of card-i
                         curCard = "#card-" + i + ">."
-                        $(curCard + "date-display").text(days[e].dt_txt.substr(0, 10)).hide().fadeIn()
-                        $(curCard + "thumb-display").attr("src","http://openweathermap.org/img/wn/" + days[e].weather[0].icon + ".png").hide().fadeIn()
-                        $(curCard + "temp-display").text("Temp: " + parseInt(days[e].main.temp) + degreeSign).hide().fadeIn()
-                        $(curCard + "humid-display").text("Humidity: " + days[e].main.humidity + " %").hide().fadeIn()
+                        // populate the Date, Weather icon, Temperature, and Humidity
+                        $(curCard + "date-display").text(days[noon].dt_txt.substr(0, 10)).hide().fadeIn()
+                        $(curCard + "thumb-display").attr("src","http://openweathermap.org/img/wn/" + days[noon].weather[0].icon + ".png").hide().fadeIn()
+                        $(curCard + "temp-display").text("Temp: " + parseInt(days[noon].main.temp) + degreeSign).hide().fadeIn()
+                        $(curCard + "humid-display").text("Humidity: " + days[noon].main.humidity + " %").hide().fadeIn()
                     }
                 })
             }
